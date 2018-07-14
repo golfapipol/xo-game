01e330c7edde63889284d1184bf4453985e6a258
Apipol Sukgler <apipol@computerlogy.com>
2018-07-14 22:32:44 +0700

    [Edited]: add implement TurnOf, IsBoardEmpty and Fill

--- xo.go
+++ xo.go
@@ -5,6 +5,47 @@ type Game struct {
 	Board   Board
 }
 
+func (g Game) TurnOf(player Player, row, col int) {
+	if g.IsBoardEmpty(row, col) {
+		g.Fill(row, col, player.Symbol)
+	}
+}
+
+func (g Game) GetBoardPosition(row, col int) State {
+	return g.Board.Slots[row][col]
+}
+
+func (g Game) IsBoardEmpty(row, col int) bool {
+	return g.Board.Slots[row][col].Symbol == ""
+}
+
+func (g Game) Fill(row, col int, symbol string) {
+	g.Board.Slots[row][col].Symbol = symbol
+}
+
+type Board struct {
+	Slots [][]State
+	Size  Size
+}
+
+type State struct {
+	Symbol string
+}
+
+type Player struct {
+	Symbol string
+}
+
+type Size struct {
+	X int
+	Y int
+}
+
+type Position struct {
+	Row int
+	Col int
+}
+
 func NewGame(playerOne, playerTwo Player, boxSize int) Game {
 	size := NewSize(boxSize)
 	board := NewBoard(size)
@@ -33,22 +74,4 @@ func NewBoard(size Size) Board {
 
 func NewSize(boxSize int) Size {
 	return Size{boxSize, boxSize}
-}
-
-type Board struct {
-	Slots [][]State
-	Size  Size
-}
-
-type State struct {
-	Symbol string
-}
-
-type Player struct {
-	Symbol string
-}
-
-type Size struct {
-	X int
-	Y int
 }
