252e598402aa148613c74c943530ab3a6231811e
Apipol Sukgler <apipol@computerlogy.com>
2018-07-14 21:46:28 +0700

    [Added]: add Test for NewPlayer

--- xo_test.go
+++ xo_test.go
@@ -3,8 +3,8 @@ package xo
 import "testing"
 
 func Test_NewGame_Input_2_Players_Should_Be_Game_With_2_Players(t *testing.T) {
-	playerOne := Player{Symbol: "O"}
-	playerTwo := Player{Symbol: "X"}
+	playerOne := NewPlayer("O")
+	playerTwo := NewPlayer("X")
 	expected := Game{
 		Players: []Player{
 			playerOne,
@@ -16,6 +16,28 @@ func Test_NewGame_Input_2_Players_Should_Be_Game_With_2_Players(t *testing.T) {
 	actual := NewGame(playerOne, playerTwo)
 
 	if len(expected.Players) != len(actual.Players) {
+		t.Errorf("Expected %v but it got %v", expected, actual)
+	}
+}
+
+func Test_NewPlayer_Input_O_Should_Be_Player_O(t *testing.T) {
+	symbol := "O"
+	expected := Player{Symbol: "O"}
+
+	actual := NewPlayer(symbol)
+
+	if expected != actual {
+		t.Errorf("Expected %v but it got %v", expected, actual)
+	}
+}
+
+func Test_NewPlayer_Input_X_Should_Be_Player_X(t *testing.T) {
+	symbol := "X"
+	expected := Player{Symbol: "X"}
+
+	actual := NewPlayer(symbol)
+
+	if expected != actual {
 		t.Errorf("Expected %v but it got %v", expected, actual)
 	}
 }
