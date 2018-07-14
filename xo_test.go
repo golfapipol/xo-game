16d7dbdd3109ed967294cc7f21591f92bb3577f1
Apipol Sukgler <apipol@computerlogy.com>
2018-07-14 21:33:59 +0700

    [Edited]: Game should have player and board

--- xo_test.go
+++ xo_test.go
@@ -2,12 +2,20 @@ package xo
 
 import "testing"
 
-func Test_NewGame_Should_Be_Game(t *testing.T) {
-	expected := Game{}
+func Test_NewGame_Input_2_Players_Should_Be_Game_With_2_Players(t *testing.T) {
+	playerOne := Player{Symbol: "O"}
+	playerTwo := Player{Symbol: "X"}
+	expected := Game{
+		Players: []Player{
+			playerOne,
+			playerTwo,
+		},
+		Board: Board{},
+	}
 
-	actual := NewGame()
+	actual := NewGame(playerOne, playerTwo)
 
-	if expected != actual {
+	if len(expected.Players) != len(actual.Players) {
 		t.Errorf("Expected %v but it got %v", expected, actual)
 	}
 }
